01b8d1170cc52b33c9faffcff78bedb0a193b092
Sahil Sinha <111557020+xOptimizer@users.noreply.github.com>
2023-10-01 17:02:06 +0000

    Operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -22,7 +22,27 @@ console.log(typeof(booleanisLoggedIn));
 //"any string " => true
 
 let someNumber = 33;
+
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+
+// *********************** OPERATIONS***********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Sahil";
+
+let str3= str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
 
