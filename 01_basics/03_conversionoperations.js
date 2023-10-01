1be1eab8928c5a8d22f4fdd4b2486f4f875a6f46
Sahil Sinha <111557020+xOptimizer@users.noreply.github.com>
2023-10-01 16:43:45 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,28 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumbere);
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1
+// null and false => 0
+
+let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(booleanisLoggedIn));
+
+// 1 => true ; 0 => false
+//"" empty string => false
+//"any string " => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
