ef8a6f1d06834c25e20f100376f1cd2162a653bf
Sahil Sinha <111557020+xOptimizer@users.noreply.github.com>
2023-10-01 15:34:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3
+    +
+      3) // code readability should be high
+
+console.log("Sahil")
+
+
+let name = "sahil"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to the power 53`
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value // rep. of empty value // null is of type object
+// undefined =>  is of type undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
